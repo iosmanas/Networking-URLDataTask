51a960f29acbc5ba50a0282882e1ba416df78a54
Manas Duishoev <manasduishoev034@gmail.com>
2023-02-03 04:51:52 +0600

    Initial Commit

---
+++ Networking-URLDataTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Networking-URLDataTask
+//
+//  Created by new on 3/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
